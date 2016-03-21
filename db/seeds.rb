04d68119f54e0d76802546841ff5464780ca9d7f
Angeline Tan <atan@gmail.com>
2016-03-21 20:29:43 +0800

    Created seed file to prepopulate the code_languages, locations, roles table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+CodeLanguage.destroy_all
+Location.destroy_all
+Role.destroy_all
+
+[
+    {name: "Javascript"},
+    {name: "Ruby"},
+    {name: "Pseudo Code"}
+].each do |language|
+  CodeLanguage.create!(language)
+end
+
+[
+    {
+        name: "GA Singapore",
+        city: "Singapore",
+        country:"Singapore"
+    },
+    {
+        name: "GA Sydney",
+        city: "Sydney",
+        country:"Australia"
+    },
+].each do |location|
+  Location.create!(location)
+end
+
+[
+    {name: "Student"},
+    {name: "Instructor"},
+    {name: "Teaching Assistant"},
+    {name: "Administrator"}
+].each do |role|
+  Role.create!(role)
+end
