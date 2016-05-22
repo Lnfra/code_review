430a6234913160260fbe95cc85238857edff2593
Angeline Tan <atan009@gmail.com>
2016-05-22 22:06:46 +0800

    Refactor all helper methods out from application controller and into application helper. Add code to show flash messages in application layout. Modify css so that flash messages are no tblocked by navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,28 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #Make current_user accessable from the view
-  helper_method :get_current_user
-  def get_current_user
-    @current_user ||= User.find(session[:user_id]) unless session[:user_id].blank?
-    return @current_user
-  end
-
-  def logged_in?
-    if get_current_user.blank?
-      false
-    else
-      true
-    end
-    # !!current_user
-  end
-
-  #Redirect to login page if user is not logged in
-  def restrict_access
-    unless logged_in?
-      flash[:error] = "YOU MUST LOGIN!"
-      redirect_to login_url
-    end
-  end
+  #Define all helper methods in ApplicationHelper
+  #Include in ApplicationController so that controllers can access these methods as well.
+  include ApplicationHelper
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,27 @@ module ApplicationHelper
     return markdown.render(text).html_safe
   end
 
+  #Make current_user accessible from the view
+  def get_current_user
+    @current_user ||= User.find(session[:user_id]) unless session[:user_id].blank?
+    return @current_user
+  end
+
+  def logged_in?
+    if get_current_user.blank?
+      false
+    else
+      true
+    end
+    # !!current_user
+  end
+
+  #Redirect to login page if user is not logged in
+  def restrict_access
+    unless logged_in?
+      flash[:error] = "You have to login to access this page."
+      redirect_to login_url
+    end
+  end
+
 end
